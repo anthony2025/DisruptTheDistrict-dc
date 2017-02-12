fa8a7c46536e6cc9a19d52d0e866e67f338ec387
Anthony Ascencio <anthony2025@gmail.com>
2017-02-11 19:56:31 -0500

    DB working!!!

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose')
 const express = require('express')()
 
 // setting up environment variables
 require('dotenv').config({silent: false})
 const PORT = process.env.PORT || 4000
-const DBURI = process.env.DBURI
-
-// setting up mongoose
-mongoose.connect(DBURI)
 
 // setting up our router
 express.use('/', require('./router'))
--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,7 @@
-const Video = require('./model')
 const fs = require('fs')
 const aws = require('aws-sdk');
 const uuid = require('node-uuid');
+const path = require('path')
 const s3 = new aws.S3();
 
 const controller = {
@@ -9,14 +9,14 @@ const controller = {
     const params = {
       Bucket: process.env.BUCKET_NAME,
       Key: uuid.v4(),
-      Body: fs.readFileSync('~/Desktop/lecture1.png'),
+      Body: fs.readFileSync(path.join(__dirname, 'lecture1.png')),
       Metadata: {
         subject: 'CS',
         class: '101',
         lecture: 'Recursion vs. Loops',
         university: 'University of Maryland',
         professor: 'Roger Thomas',
-        date: Date.now()
+        date: Date.now().toString()
       }
     }
     s3.putObject(params, (err, data) => {
